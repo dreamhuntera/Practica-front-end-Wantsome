b9611354b90d2184d3cba7796fc823390ac95534
dreamhuntered <dreamhuntera@gmail.com>
2019-10-30 01:46:42 +0200

    Tema8-Function declaration types and Condittionals-- cu exercitiu 4. 3 nu functioneaza

--- Saptamna 6/HW-JS-W2/JS2HW.js
+++ Saptamna 6/HW-JS-W2/JS2HW.js
@@ -13,33 +13,33 @@ var regexp = /^[1-9]\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])(0[1-9]|[1-4]\d|5[0
 function CNPcheck(cnp) {
     var strCnp = cnp.toString();
 
-if (strCnp.length == 0) {
-    return "is empty";
-} else {
-    if (regexp.test(cnp)) {
-        if (strCnp.charAt(0) == 1) {
-        return "Persona verificata este de sexul M";
+    if (strCnp.length == 0) {
+        return "is empty";
+    } else {
+        if (regexp.test(cnp)) {
+            if (strCnp.charAt(0) == 1) {
+                return "Persona verificata este de sexul M";
+            }
+            else if (strCnp.chart(0) == 2) {
+                return "Persona verificata este de sexul F";
+            } else if (strCnp.charAt(0) == 3) {
+                return "Persoana verificata este de sexul M";
+            } else if (strCnp.charAt(0) == 4) {
+                return "Persoana verificata este de sexul F";
+            } else if (strCnp.charAt(0) == 5) {
+                return "Persoana verificata este de sexul M";
+            } else if (strCnp.charAt(0) == 6) {
+                return "Persoana verificata este de sexul F";
+            } else if (strCnp.charAt(0) == 7 || strCnp.charAt(0) == 8) {
+                return "Persoana verificata este straina rezidenta in Romania";
+            }
+            else {
+                return "unknown gender";
+            }
+        } else {
+            return "Invalid CNP";
+        }
     }
-        else if (strCnp.chart(0) == 2) {
-        return "Persona verificata este de sexul F";
-    } else if (strCnp.charAt(0) == 3)  {
-        return "Persoana verificata este de sexul M";
-    } else if (strCnp.charAt(0) == 4) {
-        return "Persoana verificata este de sexul F";
-    } else if (strCnp.charAt(0) == 5) {
-        return "Persoana verificata este de sexul M";
-    } else if (strCnp.charAt(0) == 6) {
-        return "Persoana verificata este de sexul F";
-    } else if (strCnp.charAt(0) == 7 || strCnp.charAt(0) == 8) {
-        return "Persoana verificata este straina rezidenta in Romania";
-    }
-        else {
-        return "unknown gender";
-    }
-}   else {
-        return "Invalid CNP";
-}
-}
 };
 console.log(CNPcheck(1880714225902));
 
@@ -60,25 +60,25 @@ console.log(CNPcheck(1880714225902));
 //introduce what happens if non of the above fit--default
 //verify function by calling it in the console.log with an argument
 
-var grade = function(points) {
+var grade = function (points) {
     // if (points === /[0-9]*\d/) {
-        switch(true) {
-            case (points === 1 || points <= 3): return "Calificativul corespunzator punctajului " + points + " este " + "E";
-            break; 
-            case (points === 3 || points <= 6): return "Calificativul corespunzator punctajului " + points + " este " + "D";
+    switch (true) {
+        case (points === 1 || points <= 3): return "Calificativul corespunzator punctajului " + points + " este " + "E";
             break;
-            case (points === 7 || points <= 8): return "Calificativul corespunzator punctajului " + points + " este " + "B";
+        case (points === 3 || points <= 6): return "Calificativul corespunzator punctajului " + points + " este " + "D";
             break;
-            case (points === 9): return "Calificativul corespunzator punctajului " + points + " este " + "A";
+        case (points === 7 || points <= 8): return "Calificativul corespunzator punctajului " + points + " este " + "B";
             break;
-            case (points === 10): return "Calificativul corespunzator punctajului " + points + " este " + "A+";
+        case (points === 9): return "Calificativul corespunzator punctajului " + points + " este " + "A";
             break;
-            default: "absent";
-        }
-    };
-    // else {
-    //     return "not a point";
-    // }
+        case (points === 10): return "Calificativul corespunzator punctajului " + points + " este " + "A+";
+            break;
+        default: "absent";
+    }
+};
+// else {
+//     return "not a point";
+// }
 
 console.log(grade(4));
 
@@ -119,24 +119,62 @@ console.log(carDescription('Dacia'));
 //var2
 
 function carDescript(carType) {
-    var car = function(){ return "Marca " + carType + " se produce in " + countryFactory;}
-    switch(true) {
-        case carType === "Toyota": return(car);
-        break;
-        case carType === "Mercedes": return(car);
+    var car = function () { return "Marca " + carType + " se produce in " + countryFactory; }
+    switch (true) {
+        case carType === "Toyota": return (car);
+            break;
+        case carType === "Mercedes": return (car);
     }
 }
 
-//var3
+//var3--Object literals //NOT FINISHED-or better said NOT WORKING
 
-var carDescription = {
+function getCarDescription(manufacturer) {
+    
+    var searchIn = {
+        "Toyota": function () { return "Japan" },
+        "Mercedes": function () { return "Germania" },
+        "Dacia": function () { return "Romania" },
+        "Skoda": function () { return "Cehia" },
+        "Sangyong": function () { return "China" },
+        "Renault": function () { return "Franta" },
+        default: function () { return "Marca este necunoscuta" }
+    };
+    if (Object.keys(searchIn) !== manufacturer) {
+        return (searchIn.default());
+    } 
 
-    brand: ["Toyota", "Mercedes", "Dacia", "Skoda", "Sangyong", "Renault"],
-    countryFactory: ["Romania", "Japonia", "Cehia", "China", "Germania", "Franta"],
-    display: function () {
-        return "Marca " + this.brand + " se produce in " + this.countryFactory;
-    }
-
+        return ("Marca " + manufacturer + " se produce in " + searchIn[manufacturer]());
+    
 };
 
-console.log()
+
+console.log(getCarDescription("Dacia"));
+
+
+
+
+
+
+//ex 4 --IIFE
+
+var cars = [
+    {name: "John", salary: 20000},
+    {name: "Danny", salary: 30500},
+    {name: "Bekker", salary: 15000}
+];
+var carss = [];
+cars.sort(function(a, b) {
+    if (a.salary < b.salary){
+        return -1;
+    } else if (a.salary> b.salary) {
+        return 1;
+    } else {
+        return 0;
+    }
+});
+
+    
+ 
+console.log(cars);
+
