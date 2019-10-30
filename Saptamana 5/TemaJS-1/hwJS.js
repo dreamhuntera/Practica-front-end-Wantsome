6385d9dea17889908af89fba1ecb45e19ec31646
dreamhuntered <dreamhuntera@gmail.com>
2019-10-30 15:48:47 +0200

    Tema6- JS strings- Refacut dupa feedback.

--- Saptamana 5/TemaJS-1/hwJS.js
+++ Saptamana 5/TemaJS-1/hwJS.js
@@ -1,7 +1,7 @@
 //ex1
-function is_string(parameter1){
-    return (typeof(parameter1) === "string");
-   
+function is_string(parameter1) {
+    return (typeof (parameter1) === "string");
+
 }
 console.log(is_string('w3school'));
 console.log(is_string([1, 2, 4, 0]));
@@ -16,70 +16,77 @@ console.log(is_blank('abc'));
 
 //ex3
 function string_to_array(str) {
-    var result = str.split('');
+    var result = str.split(' ');
     return result;
 }
 console.log(string_to_array("Robin Singh"));
 
-//ex4
-function abbrev_name(str){
+//ex4- varianta doar pentru exemplu
+function abbrev_name(str) {
     var result = str.substr(0, 7) + ".";
     return result;
 }
 console.log(abbrev_name("Robin Singh"));
 
+//ex 4 varianta generala
+function abbrev_name(str) {
+    var result = str.split(' ').shift() + " " + str.split(' ').pop().substr(0, 1) + ".";
+    return result;
+}
+console.log(abbrev_name("Robin Singh"));
+
 //ex5
-function capitalize(str){
+function capitalize(str) {
     var result = str.charAt(0).toUpperCase() + str.slice(1);
     return result;
 }
-console.log (capitalize('js string exercises'));
+console.log(capitalize('js string exercises'));
 
 //ex6
 
-function truncate_string(str){
+function truncate_string(str) {
     var result = str.slice(0, 4);
     return result;
 }
 
-console.log (truncate_string("Robin Singh", 4));
+console.log(truncate_string("Robin Singh", 4));
 //ex7
-    //var1
-    function isUpperCaseAt(str, poz) {
-        return str.charAt(poz).toUpperCase() === str.charAt(poz);
-    }
-    console.log(isUpperCaseAt('Js STRING EXERCISE', 1));
+//var1
+function is_upper_case_at(str, poz) {
+    return str.charAt(poz).toUpperCase() === str.charAt(poz);
+}
+console.log(is_upper_case_at('Js STRING EXERCISE', 1));
 
-    //var 2
-    function isUpperCaseAt(str, poz) {
-        
-        return str.charAt(poz) === /[A-Z]/g;
-    }
-    console.log(isUpperCaseAt('Js STRING EXERCISES', 1)); //nu-mi iese decat cu valoarea indexului 1, da si la celelate false. Help!
+//var 2
+function is_upper_case_at(str, poz) {
+
+    return str.charAt(poz) === /[A-Z]/g;
+}
+console.log(is_upper_case_at('Js STRING EXERCISES', 1)); //nu-mi iese decat cu valoarea indexului 1, da si la celelate false. Help!
 
 //ex8
-    //var 1
-    function insert(str, strr, poz) {
-    var position = str.charAt(poz);
-        return str.replace(position, strr);
-    }
-    console.log(insert('We are doing some exercises.','JavaScript ',18));
+//var 1
+// function insert(str, strr, poz) {
+// var position = str.charAt(poz) + 1;
+//     return str.replace(position, strr);
+// }
+// console.log(insert('We are doing some exercises.','JavaScript ',18));--INCORRECT
 
-    //var 2
-    function insert(str, strr, poz) {
-        return str.substr(0, poz) + strr + str.substr(poz);
-    }
-    console.log(insert('We are doing some exercises.','JavaScript ',18));
+//var 2
+function insert(str, strr, poz) {
+    return str.substr(0, poz) + strr + str.substr(poz);
+}
+console.log(insert('We are doing some exercises.', 'JavaScript ', 18));
 
 //ex9
 function remove_first_occurence(str, strr) {
-    return str.trim(strr);
+    return str.replace(strr, "");
 }
 console.log(remove_first_occurence("The quick brown fox jumps over the lazy dog", 'the'));
 
 //ex10
 function compare_strings(str, strr) {
-    return str === strr;
+    return str.toUpperCase === strr.toUpperCase;
 }
 
 console.log(compare_strings('abcd', 'AbcD'));
@@ -88,11 +95,11 @@ console.log(compare_strings('abcd', 'AbcD'));
 //     return str.test() === strr.test();}
 //     console.log(compare_strings('abcd', 'AbcD'));
 
-    //nu merge, ofc
+//nu merge, ofc
 
 //ex11
 function Uncapitalize(txt) {
-    return txt.substr(0,1).toLowerCase() + txt.substr(1);
+    return txt.substr(0, 1).toLowerCase() + txt.substr(1);
 
 }
 console.log(Uncapitalize('Js string exercises'));
